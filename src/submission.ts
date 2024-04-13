bf196c671fa5f48d9adc6cd54d010ee5a7bd40ef
Carys Chan <z5379637@ad.unsw.edu.au>
2024-04-13 20:31:09 +1000

    added https

--- src/submission.ts
+++ src/submission.ts
@@ -1,4 +1,4 @@
 // The URL can be anything you want, as long as it contains your zID exactly once.
 // For example, 'https://z1234444-forum-deploy-test.vercel.app'
 // The URL must begin with 'https://'
-export const DEPLOYED_URL = 'z5379637-lab09-deploy-xi.vercel.app';
+export const DEPLOYED_URL = 'https://z5379637-lab09-deploy-xi.vercel.app';
